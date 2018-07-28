5d1ba002dd970cc138fa334aabd49669fb4b41c7
Nabeel <38478892+nabeelmumtaz2@users.noreply.github.com>
2018-07-28 18:04:07 -0400

    Update nab.sql

--- Weather Observation Station 3/nab.sql
+++ Weather Observation Station 3/nab.sql
@@ -1,1 +1,2 @@
+--Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, but must exclude duplicates from your answer.
 Select DISTINCT CITY from STATION where MOD(id , 2) = 0;
